373576a37ab708dcc47d3b8bff3a4075e5f2069d
Timur Mangliev <tigrmango@gmail.com>
2023-01-08 22:14:40 +1000

    small logic fix, some logs

--- refan.cpp
+++ refan.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 #include <math.h>
 #include <csignal>
+#include <getopt.h>
 
 inline float map(float x, float in_min, float in_max, float out_min, float out_max){
     return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
@@ -33,7 +34,7 @@ enum class Verbosity {
     DEBUG
 };
 
-Verbosity global_verbosity = Verbosity::WARNING;
+Verbosity global_verbosity = Verbosity::DEBUG;
 
 void log(std::string message, Verbosity v){
     time_t tt;
@@ -62,16 +63,16 @@ void log(std::string message, Verbosity v){
 
 typedef struct Fan {
     std::string* name               = nullptr;
-    int* min_pwm_temp               = nullptr;
-    int* max_pwm_temp               = nullptr;
-    std::string* temp_sensor_path   = nullptr;
+    std::string* temp_input_path    = nullptr;
+    std::string* pwm_control_path   = nullptr;
+    std::string* pwm_read_path      = nullptr;
+    std::string* pwm_mode_path      = nullptr;
+    int* min_temp                   = nullptr;
+    int* max_temp                   = nullptr;
     int* min_pwm                    = nullptr;
     int* max_pwm                    = nullptr;
     int* start_pwm                  = nullptr;
     int* stop_pwm                   = nullptr;
-    std::string* pwm_control_path   = nullptr;
-    std::string* pwm_read_path      = nullptr;
-    std::string* pwm_mode_path      = nullptr;
     bool* stopped                   = nullptr;
 } Fan;
 
@@ -85,19 +86,30 @@ std::string current_section = "null";
 static int dispatch(IniDispatch* const dispatch, void* ){
     if(dispatch->type == INI_SECTION) {
         std::string section_candidate = dispatch->data;
-        // if ( !(((long long)current_section.find("Fan"))<0) || !(((long long)current_section.find("fan"))<0) ) {
-        //     Fan* last = fans.back();
-        //     if (fans.size()>0 && (last->max_pwm_temp == nullptr || last->min_pwm_temp == nullptr || 
-        //                           last->temp_sensor_path == nullptr || last->pwm_control_path || last->pwm_read_path || 
-        //                           last->max_pwm == nullptr || last->min_pwm == nullptr || last->start_pwm == nullptr || last->stop_pwm == nullptr)) {
-        //     std::cerr << "Fan \"" << current_section << "\"initialisation incomplete, likely because of invalid or missing values" << "\n" << last;
-        //     std::cerr << &last;
-        //     }
-        // }
+        if ( !(((long long)current_section.find("Fan"))<0) || !(((long long)current_section.find("fan"))<0) ) {
+            Fan* last = fans.back();
+            if (fans.size()>0 && (
+                                last->max_pwm == nullptr ||
+                                last->min_pwm == nullptr ||
+                                last->max_temp == nullptr ||
+                                last->min_temp == nullptr ||
+                                last->start_pwm == nullptr ||
+                                last->stop_pwm == nullptr ||
+                                last->temp_input_path == nullptr ||
+                                last->pwm_control_path == nullptr ||
+                                last->pwm_read_path == nullptr ||
+                                last->pwm_mode_path == nullptr
+                                ))
+            {
+            log(std::string("Fan \"") + current_section + "\"initialisation incomplete, likely because of invalid or missing values in config file\n", Verbosity::ERROR);
+            log("Not trying to fill empty values automatically because logic isn't implemented yet", Verbosity::FATAL);
+            return -1;
+            }
+        }
         if (!(((long long)section_candidate.find("Fan"))<0) || !(((long long)section_candidate.find("fan"))<0) ) {
             fans.push_back(new Fan());
             fans.back()->name = new std::string(section_candidate);
-            fans.back()->stopped = new bool;
+            fans.back()->stopped = new bool(false);
         }
         current_section = section_candidate;
 
@@ -129,10 +141,10 @@ static int dispatch(IniDispatch* const dispatch, void* ){
                 fans.back()->start_pwm = new int(ini_get_int(dispatch->value));
 
             if(data == "min_pwm_temp")
-                fans.back()->min_pwm_temp = new int(ini_get_int(dispatch->value));
+                fans.back()->min_temp = new int(ini_get_int(dispatch->value));
 
             if(data == "max_pwm_temp")
-                fans.back()->max_pwm_temp = new int(ini_get_int(dispatch->value));
+                fans.back()->max_temp = new int(ini_get_int(dispatch->value));
 
             if(data == "pwm_control_path")
                 fans.back()->pwm_control_path = new std::string(dispatch->value);
@@ -144,7 +156,7 @@ static int dispatch(IniDispatch* const dispatch, void* ){
                 fans.back()->pwm_mode_path = new std::string(dispatch->value);
 
             if(data == "temp_sensor_path")
-                fans.back()->temp_sensor_path = new std::string(dispatch->value);
+                fans.back()->temp_input_path = new std::string(dispatch->value);
         }
     }
 
@@ -153,7 +165,7 @@ static int dispatch(IniDispatch* const dispatch, void* ){
 }
 
 int fanHandler(Fan* fan){
-    std::ifstream temp(*fan->temp_sensor_path, std::ios_base::in);
+    std::ifstream temp(*fan->temp_input_path, std::ios_base::in);
     std::ifstream validate(*fan->pwm_read_path, std::ios_base::in);    
     std::ofstream control(*fan->pwm_control_path, std::ios_base::out);
 
@@ -161,25 +173,34 @@ int fanHandler(Fan* fan){
     temp.read(temp_c, 16);
     float temp_f = atof(temp_c)/1000;
     temp.close();
-    int pwm = clamp(round(map(temp_f, *fan->min_pwm_temp, *fan->max_pwm_temp, *fan->min_pwm, *fan->max_pwm)), *fan->min_pwm, *fan->max_pwm);
-    // std::cout << "raw " << *fan->name << " pwm value=" << pwm << "\n";
+    //logic breakdown:
+    //fan should start spinning at start_pwm power as soon as temp_f > min_temp
+    //continue spinning, adjusting the power as temp_f fluctuates
+    //and stop/slow down as soon as temperature has fallen enough for the pwm value to become less than stop_pwm
+
+    int pwm = clamp(round(map(temp_f, *fan->min_temp, *fan->max_temp, *fan->start_pwm, *fan->max_pwm)), *fan->min_pwm, *fan->max_pwm);
+
     if(*fan->stopped){
-        if(!(pwm>*fan->start_pwm))
+        if(!(pwm>=*fan->start_pwm)){
             pwm=0;
-        else 
+        }
+        else{ 
             *fan->stopped = false;
+            log("Started " + *fan->name, Verbosity::DEBUG);
+        }
     }
 
     if(!*fan->stopped){
-        if(pwm<*fan->stop_pwm){
+        if(pwm<=*fan->stop_pwm){
             pwm=0;
             *fan->stopped = true;
-            // std::cout << "stopped " << *fan->name << "\n";
+            log("Stopped " + *fan->name, Verbosity::DEBUG);
         }
 
     }
-    // std::cout << "final " << *fan->name << " pwm value=" << pwm << "\n";
-    control.write(std::to_string(pwm).c_str(), 16);
+
+    log("name: " + *fan->name + " pwm: " + std::to_string(pwm) + " start_pwm: " + std::to_string(*fan->start_pwm) + " stop_pwm: " + std::to_string(*fan->stop_pwm) + " stopped: " + std::to_string(*fan->stopped), Verbosity::DEBUG);
+    control.write(std::to_string(pwm).c_str(), std::to_string(pwm).length()); //Write pwm value
     control.close();
     if (control.bad()) {
         log(*fan->name + " write failure, exiting", Verbosity::FATAL);
@@ -188,7 +209,7 @@ int fanHandler(Fan* fan){
 
     char validate_c[16];
     validate.read(validate_c, 16);
-    if (!(pwm == atof(validate_c))){
+    if (!(pwm == atof(validate_c))){ //Validate pwm value write
         log(*fan->name + " write validation failure, exiting", Verbosity::FATAL);
         return -1;
     }
@@ -213,7 +234,7 @@ void reset_fans(int sig){
 }
 
 int main(int argc, char** argv){
-    // getopt(argc, argv, "v");
+    int nr = 1;
     if(argc == 1){
         log("No configuration file provided. exiting", Verbosity::FATAL);
         exit(-1);
@@ -224,16 +245,20 @@ int main(int argc, char** argv){
         exit(-2);
     }
 
-    std::atexit(&reset_fans);
-    signal(SIGINT, &reset_fans);
-    signal(SIGSEGV, &reset_fans);
-    signal(SIGTERM, &reset_fans);
-    signal(SIGABRT, &reset_fans);
-    signal(SIGQUIT, &reset_fans);
-    signal(SIGKILL, &reset_fans);
-    signal(SIGPIPE, &reset_fans);
+    //trying hard to set the fans to full speed regardless of circumstance
+    if (nr>=0) {
+        std::atexit(&reset_fans);
+        signal(SIGINT, &reset_fans);
+        signal(SIGSEGV, &reset_fans);
+        signal(SIGTERM, &reset_fans);
+        signal(SIGABRT, &reset_fans);
+        signal(SIGQUIT, &reset_fans);
+        signal(SIGKILL, &reset_fans);
+        signal(SIGPIPE, &reset_fans);
+    }
 
-    for (Fan* fan : fans) {
+
+    for (Fan* fan : fans) { //enable manual control
         std::ofstream mode(*fan->pwm_mode_path, std::ios_base::out);
         mode.write("1", 2);
     }
